e19c222c9d321ccf8df789b0ff545b9657e2da60
MeppyNep <81493305+MeppyNep@users.noreply.github.com>
2021-03-27 21:43:37 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,10 @@
 import discord
+from discord.ext import commands
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as', self.user)
+bot = commands.Bot(command_prefix='>')
 
-    async def on_message(self, message):
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
 
-        if message.content == 'ping':
-            await message.channel.send('pong')
-
-client = MyClient()
-client.run('token')
+bot.run('token')
